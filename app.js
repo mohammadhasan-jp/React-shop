6d237b60b8bd23272fe9969fc29398ec17bb6438
mohammadhasanjp <76821234+mohammadhasan-jp@users.noreply.github.com>
2022-04-22 16:02:28 +0430

    sign up form

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 alert('hello')
+
+class signup extends Component {
+    state = {  } 
+    render() { 
+        return (<>
+        <i class="fas fa-h1    "></i>
+        </>);
+    }
+}
+ 
+export default sign up;
