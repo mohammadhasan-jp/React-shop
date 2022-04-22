c4b3413e2d138ea16b0501a600b61c18100e1e19
mohammadhasanjp <76821234+mohammadhasan-jp@users.noreply.github.com>
2022-04-22 16:01:15 +0430

    login form

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 alert('hello')
+class login extends Component {
+    state = {  } 
+    render() { 
+        return (<>
+        <i class="fas fa-h1    "></i>
+        
+        </>);
+    }
+}
+ 
+export default login;
